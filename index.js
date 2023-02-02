b71315618940db3db8bada74a5ddde7d0d5ccf6d
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2023-02-02 21:21:35 +0200

    adds crone

--- index.js
+++ index.js
@@ -4,92 +4,95 @@ import dotenv from "dotenv";
 import path from "path";
 import puppeteer from "puppeteer";
 import Pushover from "node-pushover";
+import cron from "node-cron";
 
-(async () => {
-  /* Loading the environment variables from the .env file. */
-  const { __dirname } = getPaths(import.meta.url);
-  dotenv.config({ path: path.join(__dirname, "./.env") });
+cron.schedule("* * 2 * *", () => {
+  (async () => {
+    /* Loading the environment variables from the .env file. */
+    const { __dirname } = getPaths(import.meta.url);
+    dotenv.config({ path: path.join(__dirname, "./.env") });
 
-  /* Clear the cookies.json file. */
-  await fs.writeFile("./cookies.json", JSON.stringify([], null, 2));
+    /* Clear the cookies.json file. */
+    await fs.writeFile("./cookies.json", JSON.stringify([], null, 2));
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const siteURL = process.env.SITE_URL || "https://fuel.avias.ua/";
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const siteURL = process.env.SITE_URL || "https://fuel.avias.ua/";
 
-  await page.goto(siteURL);
+    await page.goto(siteURL);
 
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
 
-  /* Waiting for the phone input field to appear and then typing the phone into it. */
-  const searchPhoneSelector = "input[name='phone']";
-  await page.waitForSelector(searchPhoneSelector);
-  await page.type(searchPhoneSelector, process.env.PHONE_NUMBER);
+    /* Waiting for the phone input field to appear and then typing the phone into it. */
+    const searchPhoneSelector = "input[name='phone']";
+    await page.waitForSelector(searchPhoneSelector);
+    await page.type(searchPhoneSelector, process.env.PHONE_NUMBER);
 
-  /* Waiting for the password input field to appear and then typing the password into it. */
-  const searchPasswordSelector = "input[name='static_password']";
-  await page.waitForSelector(searchPasswordSelector);
-  await page.type(searchPasswordSelector, process.env.PASSWORD);
+    /* Waiting for the password input field to appear and then typing the password into it. */
+    const searchPasswordSelector = "input[name='static_password']";
+    await page.waitForSelector(searchPasswordSelector);
+    await page.type(searchPasswordSelector, process.env.PASSWORD);
 
-  /* Clicking the button to submit form. */
-  const confirmBtnSelector = "button[type='button']";
-  await page.waitForSelector(confirmBtnSelector);
-  await page.click(confirmBtnSelector);
+    /* Clicking the button to submit form. */
+    const confirmBtnSelector = "button[type='button']";
+    await page.waitForSelector(confirmBtnSelector);
+    await page.click(confirmBtnSelector);
 
-  /* Waiting for the login page to load. */
-  const loginSelector = ".login";
-  await page.waitForSelector(loginSelector);
+    /* Waiting for the login page to load. */
+    const loginSelector = ".login";
+    await page.waitForSelector(loginSelector);
 
-  /* Saving the cookies to a file. */
-  const cookies = await page.cookies();
-  await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
+    /* Saving the cookies to a file. */
+    const cookies = await page.cookies();
+    await fs.writeFile("./cookies.json", JSON.stringify(cookies, null, 2));
 
-  /* Creating a new instance of the Pushover class. */
-  const push = new Pushover({
-    token: process.env.PUSHOVER_TOKEN,
-    user: process.env.PUSHOVER_USER,
-  });
+    /* Creating a new instance of the Pushover class. */
+    const push = new Pushover({
+      token: process.env.PUSHOVER_TOKEN,
+      user: process.env.PUSHOVER_USER,
+    });
 
-  const sKey = await getCookie("skey2");
-  if (sKey) {
-    try {
-      const region = process.env.REGION || "SU";
-      const gas_station = process.env.GAS_STATION || "B44";
-      const { data } = await axios.get(
-        `${siteURL}client_api/v1/1/user_accounts/1591416/stations/_${region}00${gas_station}?mode=show&_lang=uk`,
-        {
-          headers: {
-            cookie: `skey2=${sKey}`,
-          },
+    const sKey = await getCookie("skey2");
+    if (sKey) {
+      try {
+        const region = process.env.REGION || "SU";
+        const gas_station = process.env.GAS_STATION || "B44";
+        const { data } = await axios.get(
+          `${siteURL}client_api/v1/1/user_accounts/1591416/stations/_${region}00${gas_station}?mode=show&_lang=uk`,
+          {
+            headers: {
+              cookie: `skey2=${sKey}`,
+            },
+          }
+        );
+        const {
+          data: { units },
+        } = data;
+
+        const fuel = process.env.FUEL || "95";
+
+        if (units && units[fuel]) {
+          const { rate, status } = units[fuel];
+          if (status === "ok") {
+            return push.send(
+              "Avias fuel",
+              `Fuel ${fuel} is available \n Price: ${rate.toFixed(2)} UAH`
+            );
+          }
         }
-      );
-      const {
-        data: { units },
-      } = data;
-
-      const fuel = process.env.FUEL || "95";
-
-      if (units && units[fuel]) {
-        const { rate, status } = units[fuel];
-        if (status === "ok") {
-          return push.send(
-            "Avias fuel",
-            `Fuel ${fuel} is available \n Price: ${rate.toFixed(2)} UAH`
-          );
-        }
+      } catch (error) {
+        console.log(error);
+        return await browser.close();
       }
-    } catch (error) {
-      console.log(error);
-      return await browser.close();
+    } else {
+      console.log("No skey2 cookie found");
+      await browser.close();
     }
-  } else {
-    console.log("No skey2 cookie found");
-    await browser.close();
-  }
 
-  await browser.close();
-})();
+    await browser.close();
+  })();
+});
 
 /**
  * It takes a file URL and returns an object with the file's pathname and its directory name
